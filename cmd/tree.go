9c57cbe1ec9eee52e8b22309e2807de817b8a022
Daniel Stamer <dan@hello-world.sh>
2020-03-30 23:17:09 +0200

    add tree command
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

---
+++ cmd/tree.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	tresor "github.com/helloworlddan/tresor/lib"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/xlab/treeprint"
+)
+
+var treeCmd = &cobra.Command{
+	Use:   "tree",
+	Short: "List remote directory in a tree-like structure.",
+	Long:  `List remote directory in a tree-like structure.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Check for correct number of arguments
+		prefixFilter := ""
+		if len(args) == 1 {
+			prefixFilter = args[0]
+		}
+
+		attrs, err := tresor.QueryStorage(viper.Get("bucket").(string), prefixFilter)
+		if err != nil {
+			fail(err)
+		}
+
+		root := treeprint.New()
+		for _, attr := range attrs {
+			attach(root, attr.Name)
+		}
+		fmt.Println(root.String())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(treeCmd)
+}
+
+func attach(tree treeprint.Tree, path string) treeprint.Tree {
+	if path == "" {
+		return tree
+	}
+	pieces := strings.Split(path, "/")
+	sub := tree.FindByValue(pieces[0])
+	if sub == nil {
+		sub = tree.AddBranch(pieces[0])
+	}
+	return attach(sub, strings.Join(pieces[1:], "/"))
+}
--- go.mod
+++ go.mod
@@ -4,12 +4,10 @@ go 1.13
 
 require (
 	cloud.google.com/go v0.38.0
-	github.com/giantswarm/go.crypto v0.0.0-20151015074924-d5be63d84f08
-	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/proglottis/gpgme v0.1.1
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.6.2
+	github.com/xlab/treeprint v1.0.0
 	go.opencensus.io v0.22.3 // indirect
 	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	google.golang.org/api v0.17.0
